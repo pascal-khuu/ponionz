d51a1f9f7c637b3f337739fc2aaea084952a3614
pascal-khuu <81921513+pascal-khuu@users.noreply.github.com>
2021-05-28 11:55:14 +0200

    Projet evaluation ponionz

---
+++ ponionz/src/main/java/fr/formation/application/PonionzApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonionzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonionzApplication.class, args);
+	}
+
+}
